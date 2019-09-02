53cc21bcbb09af49fd5530f1828ad0b3226e4eae
= <=abdoelsimy123@gmail.com>
2019-09-02 11:55:20 +0200

    prepare project files and install required packages

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('view engine', 'ejs');
+
+app.listen(3000, (err) => {
+    console.log('server listen to port 3000');
+});
