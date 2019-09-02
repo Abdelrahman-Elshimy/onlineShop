b72431e9604235d08a0c1aabdad109f6cbe8ca05
= <=abdoelsimy123@gmail.com>
2019-09-02 12:46:16 +0200

    make navbar and devide page parts into header , footer and navbar

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'assets')));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
 app.listen(3000, (err) => {
     console.log('server listen to port 3000');
 });
